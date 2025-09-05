dd02a6f640b4c0abbf0b035a02064c92c756f2b9
Krista Singleton <krista.singleton@intimetec.com>
2025-09-05 14:14:28 -0600

    Added logic for auth setup in frontend service

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,1 +1,22 @@
-export {};
+const API_BASE = "http://localhost:5000";
+
+export const registerUser = async (username: string, password: string) => {
+    const res = await fetch(`${API_BASE}/auth/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({username, password})
+    });
+    if (!res.ok) throw new Error(await res.text());
+};
+
+export const loginUser = async (username: string, password: string) => {
+    const res = await fetch(`${API_BASE}/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({username, password }),
+    });
+    if (!res.ok) throw new Error(await res.text());
+
+    const data = await res.json();
+    localStorage.setItem("token", data.token);
+}
